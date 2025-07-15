9d1d06028a41a8a68de259606bc21d68b6b2963e
topibaja55 <usamahalhanif2024@gmail.com>
2025-07-15 09:31:16 +0700

    Create links.js

---
+++ links.js
@@ -0,0 +1,14 @@
+var links = [
+  {
+    text: "Dana Kaget Hari Ini",
+    url: "https://produk.com/link1"
+  },
+  {
+    text: "Dana Kaget Hari Ini 2",
+    url: "https://produk.com/link1"
+  },
+  {
+    text: "Dana Kaget Hari Ini 3",
+    url: "https://produk.com/link1"
+  }
+];
